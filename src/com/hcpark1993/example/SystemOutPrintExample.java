50553ad8608055d01b9fc2b37e0e1ba133782669
Engineeringcode <engineeringcode93@gmail.com>
2022-08-31 09:04:36 +0900

    설명을 한국어로 수정

--- src/com/hcpark1993/example/SystemOutPrintExample.java
+++ src/com/hcpark1993/example/SystemOutPrintExample.java
@@ -5,20 +5,20 @@ public class SystemOutPrintExample {
 	 * https://docs.oracle.com/javase/8/docs/api/java/io/PrintStream.html
 	 */
 	public static void main(String[] args){
-		String stringSample = "sample";
+		String stringSample = "예제";
 		int intSample = 1;
 		double doubleSample = 1.12;
 		
-		System.out.println("print texts with new line.");
-		System.out.println("print texts with new line. "+stringSample+" "+intSample+" "+doubleSample);
-		System.out.print("Apple"); // No new line
-		System.out.print("Banana"+intSample);
-		System.out.println(); // Print new line
+		System.out.println("개행문자와 함께 입력된 값을 출력한다.");
+		System.out.println("개행문자와 함께 입력된 값을 출력한다. "+stringSample+" "+intSample+" "+doubleSample);
+		System.out.print("사과"); // 개행문자가 없다
+		System.out.print("바나나"+intSample);
+		System.out.println(); // 개행한다.
 		/*
-		 * format list: https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax
-		 * '\r' and '\n' are new line keywords.
+		 * 형식 목록: https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax
+		 * '\r'과 '\n', '\r\n'은 개행문자입니다.
 		 */
-		System.out.printf("Decimal:%d, Decimal:%d, Hexadecimal: %x, Hexadecimal: %x \r\n", 123, 0x123, 0x123, 123);
-		System.out.printf("String: %s, Decimal:%d, Decimal floating point: %f", stringSample, intSample, doubleSample);
+		System.out.printf("십진수:%d, 십진수:%d, 16진수: %x, 16진수: %x \r\n", 123, 0x123, 0x123, 123);
+		System.out.printf("문자열: %s, 십진수:%d, 십진수 부동 소수점: %f", stringSample, intSample, doubleSample);
 	}
 }
