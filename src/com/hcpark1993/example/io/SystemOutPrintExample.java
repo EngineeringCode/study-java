b5671a35e34cc141d83a06319fde79307808bbe3
Engineeringcode <engineeringcode93@gmail.com>
2022-10-24 12:41:17 +0900

    예제 위치 이동 및 주석 보충

--- src/com/hcpark1993/example/io/SystemOutPrintExample.java
+++ src/com/hcpark1993/example/io/SystemOutPrintExample.java
@@ -1,19 +1,27 @@
-package com.hcpark1993.example;
+package com.hcpark1993.example.io;
 
 public class SystemOutPrintExample {
 	/*
 	 * https://docs.oracle.com/javase/8/docs/api/java/io/PrintStream.html
+	 * https://docs.oracle.com/en/java/javase/14/docs/api/java.base/java/lang/System.html#out
 	 */
 	public static void main(String[] args){
+		System.out.println("개행문자와 함께 입력된 값을 출력한다.");
+		System.out.print("개행문자 없이");
+		System.out.print("입력된 값을 출력한다.");
+		/* \r\n 은 개행문자이다. */
+		System.out.print("\r\n");
+		
+		/* 변수 출력 */
 		String stringSample = "예제";
 		int intSample = 1;
 		double doubleSample = 1.12;
 		
-		System.out.println("개행문자와 함께 입력된 값을 출력한다.");
 		System.out.println("개행문자와 함께 입력된 값을 출력한다. "+stringSample+" "+intSample+" "+doubleSample);
-		System.out.print("사과"); // 개행문자가 없다
-		System.out.print("바나나"+intSample);
+		System.out.print("바나나 "+intSample+"개");
 		System.out.println(); // 개행한다.
+		System.out.printf("십진수 출력: %d \r\n", intSample);
+		
 		/*
 		 * 형식 목록: https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax
 		 * '\r'과 '\n', '\r\n'은 개행문자입니다.
