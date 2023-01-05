129aa9c780fd562c34dfd7752e00fb866ee6068b
Engineeringcode <engineeringcode93@gmail.com>
2023-01-05 12:47:51 +0900

    주석 기능 사용 예제

---
+++ src/com/hcpark1993/example/comment/CommentExample.java
@@ -0,0 +1,11 @@
+package com.hcpark1993.example.comment;
+
+public class CommentExample {
+	/*
+	 * 이 프로그램은 자바의 주석 기능을 설명하기 위해 만들었습니다.
+	 */
+	public static void main(String[] args){
+		// 문자열 'Hello World'를 표준출력에 출력합니다.
+		System.out.println("Hello World!");
+	}
+}
