2ca061d1ef382a3988e13c73f16bba00794847c2
EngineeringCode <engineeringcode93@gmail.com>
2023-08-04 23:17:28 +0900

    형변환 예제 작성

---
+++ src/com/hcpark1993/example/datatype/TypeConversionExample.java
@@ -0,0 +1,38 @@
+package com.hcpark1993.example.datatype;
+
+/*
+ * Chapter 5. Conversions and Contexts: https://docs.oracle.com/javase/specs/jls/se17/html/jls-5.html
+ */
+public class TypeConversionExample {
+
+	public static void main(String[] args){
+		float floatValue = 100.0f;
+		int intValue = 100;
+		
+		if(floatValue == (float) intValue) {
+			System.out.println("같습니다.");
+		}
+		
+		String stringValue = "100";
+		
+		if(stringValue.equals(intValue)) {
+			System.out.println("같습니다.");
+		} else {
+			System.out.println("다릅니다.");
+		}
+		
+		String stringConvert = String.valueOf(intValue);
+		if(stringValue.equals(stringConvert)) {
+			System.out.println("같습니다.");
+		} else {
+			System.out.println("다릅니다.");
+		}
+		
+		int intConvert = Integer.parseInt(stringValue);
+		if(intValue == intConvert) {
+			System.out.println("같습니다.");
+		} else {
+			System.out.println("다릅니다.");
+		}
+	}
+}
