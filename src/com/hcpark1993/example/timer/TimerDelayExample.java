52cf3d1641fd595980137628e2d6626f42ca7031
EngineeringCode <engineeringcode93@gmail.com>
2023-08-04 23:23:27 +0900

    자바 17 기준으로 갱신

--- src/com/hcpark1993/example/timer/TimerDelayExample.java
+++ src/com/hcpark1993/example/timer/TimerDelayExample.java
@@ -2,11 +2,12 @@ package com.hcpark1993.example.timer;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+/*
+ * https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Timer.html
+ */ 
 public class TimerDelayExample {
-	/*
-	 * https://docs.oracle.com/javase/8/docs/api/java/util/Timer.html
-	 * 
+	/* 
+	 * 1초 후에 수행한다.
 	 * Perform after 1000 ms.
 	 */
 	public static void main(String[] args){
