b2514feff096f77c072ee7c498f673900c6bdb53
EngineeringCode <engineeringcode93@gmail.com>
2023-08-04 23:18:47 +0900

    참고 문서 정보 추가

--- src/com/hcpark1993/example/comment/CommentExample.java
+++ src/com/hcpark1993/example/comment/CommentExample.java
@@ -1,5 +1,8 @@
 package com.hcpark1993.example.comment;
 
+/*
+ * 3.7. Comments: https://docs.oracle.com/javase/specs/jls/se17/html/jls-3.html#jls-3.7
+ */
 public class CommentExample {
 	/*
 	 * 이 프로그램은 자바의 주석 기능을 설명하기 위해 만들었습니다.
