df6c4c749bcafa793229be01278765f83c57d5d3
EngineeringCode <engineeringcode93@gmail.com>
2023-08-12 00:08:29 +0900

    do while 예제 작성

---
+++ src/com/hcpark1993/example/statements/DoWhileExample.java
@@ -0,0 +1,22 @@
+package com.hcpark1993.example.statements;
+
+/*
+ * do Statements : https://docs.oracle.com/javase/specs/jls/se17/html/jls-6.html#jls-6.3.2.4
+ */
+public class DoWhileExample {
+	
+	public static void main(String[] args){
+		int a = 3;
+		int b = 4;
+		
+		do {
+			System.out.println("변수 a의 현재값: " + a);
+			System.out.println("변수 b의 현재값: " + b);
+			a = a +1;
+		} while(a <= b);
+		
+		System.out.println("변수 a의 최종값: " + a);
+		System.out.println("변수 b의 최종값: " + b);
+	}
+	
+}
