bffdc2c2e52a859ab1fe68785155cc360d372037
EngineeringCode <engineeringcode93@gmail.com>
2023-08-12 00:07:14 +0900

    while 예제 작성

---
+++ src/com/hcpark1993/example/statements/WhileExample.java
@@ -0,0 +1,22 @@
+package com.hcpark1993.example.statements;
+
+/*
+ * 6.3.2.3: https://docs.oracle.com/javase/specs/jls/se17/html/jls-6.html#jls-6.3.2.3
+ */
+public class WhileExample {
+	
+	public static void main(String[] args){
+		int a = 3;
+		int b = 4;
+		
+		while(a <= b) {
+			System.out.println("변수 a의 현재값: " + a);
+			System.out.println("변수 b의 현재값: " + b);
+			a = a +1;
+		}
+		
+		System.out.println("변수 a의 최종값: " + a);
+		System.out.println("변수 b의 최종값: " + b);
+	}
+	
+}
