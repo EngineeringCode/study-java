f48e5cb32ae3cef7b511477adbdb2c78270b1750
EngineeringCode <engineeringcode93@gmail.com>
2023-08-12 00:10:30 +0900

    for 예제 작성

---
+++ src/com/hcpark1993/example/statements/ForExample.java
@@ -0,0 +1,21 @@
+package com.hcpark1993.example.statements;
+
+/*
+ * while Statements: https://docs.oracle.com/javase/specs/jls/se17/html/jls-6.html#jls-6.3.2.3
+ */
+public class ForExample {
+	
+	public static void main(String[] args){
+		int a;
+		int b;
+		
+		for(a = 3, b = 4; a <= b ; a = a + 1) {
+			System.out.println("변수 a의 현재값: " + a);
+			System.out.println("변수 b의 현재값: " + b);
+		}
+		
+		System.out.println("변수 a의 최종값: " + a);
+		System.out.println("변수 b의 최종값: " + b);
+	}
+	
+}
