1c965cb750bf0ffb6dda838925cd5076c9756413
EngineeringCode <engineeringcode93@gmail.com>
2023-10-16 22:34:20 +0900

    열겨형 예제 추가

---
+++ src/com/hcpark1993/example/enumeration/EnumerationExample.java
@@ -0,0 +1,13 @@
+package com.hcpark1993.example.enumeration;
+
+/*
+ * Enum Classes: https://docs.oracle.com/javase/specs/jls/se17/html/jls-8.html#jls-8.9
+ */
+public class EnumerationExample {
+	static final int KEY = 123456;
+	
+	public static void main(String[] args){
+		System.out.println("KEY: " + KEY);
+	}
+	
+}
