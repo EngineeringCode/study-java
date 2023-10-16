f34beff87b66fa2d449b847fc3b56173e3733189
engineeringcode93@gmail.com <engineeringcode93@gmail.com>
2023-10-16 22:47:09 +0900

    예제를 반대로 입력해서 수정

--- src/com/hcpark1993/example/constant/ConstantExample.java
+++ src/com/hcpark1993/example/constant/ConstantExample.java
@@ -4,30 +4,9 @@ package com.hcpark1993.example.constant;
  * Field (Constant) Declarations: https://docs.oracle.com/javase/specs/jls/se17/html/jls-9.html#jls-9.3
  */
 public class ConstantExample {
-	static enum Seasons {
-		SPRING, SUMMER, FALL, WINTER
-	}
+	static final int KEY = 123456;
 	
 	public static void main(String[] args){
-		Seasons season = Seasons.SPRING;
-		
-		switch (season) {
-			case SPRING: {
-				System.out.println("봄입니다.");
-				break;
-			}
-			case SUMMER: {
-				System.out.println("여름입니다.");
-				break;
-			}
-			case FALL: {
-				System.out.println("가을입니다.");
-				break;
-			}
-			case WINTER: {
-				System.out.println("겨울입니다.");
-				break;
-			}
-		}
+		System.out.println("KEY: " + KEY);
 	}
 }
--- src/com/hcpark1993/example/enumeration/EnumerationExample.java
+++ src/com/hcpark1993/example/enumeration/EnumerationExample.java
@@ -4,10 +4,31 @@ package com.hcpark1993.example.enumeration;
  * Enum Classes: https://docs.oracle.com/javase/specs/jls/se17/html/jls-8.html#jls-8.9
  */
 public class EnumerationExample {
-	static final int KEY = 123456;
+	static enum Seasons {
+		SPRING, SUMMER, FALL, WINTER
+	}
 	
 	public static void main(String[] args){
-		System.out.println("KEY: " + KEY);
+		Seasons season = Seasons.SPRING;
+		
+		switch (season) {
+			case SPRING: {
+				System.out.println("봄입니다.");
+				break;
+			}
+			case SUMMER: {
+				System.out.println("여름입니다.");
+				break;
+			}
+			case FALL: {
+				System.out.println("가을입니다.");
+				break;
+			}
+			case WINTER: {
+				System.out.println("겨울입니다.");
+				break;
+			}
+		}
 	}
 	
 }
